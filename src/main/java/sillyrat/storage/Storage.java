67bb7368393c217df0efa63b2984a6ada06250b5
Zixuan224 <shengzixuan224@gmail.com>
2026-02-14 17:19:25 +0800

    Add Javadoc headers for Storage class

--- src/main/java/sillyrat/storage/Storage.java
+++ src/main/java/sillyrat/storage/Storage.java
@@ -12,9 +12,17 @@ import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Storage class to save tasks added and load tasks from the file.
+ */
+
 public class Storage {
     private final Path filePath;
 
+    /**
+     * Constructor for Storage class.
+     * @param filePath Path to the storage file
+     */
     public Storage(String filePath) {
         this.filePath = Paths.get(filePath);
     }
@@ -29,6 +37,12 @@ public class Storage {
         }
     }
 
+    /**
+     * Loads tasks from the storage file.
+     * This method reads tasks from the file and convert them to appropriate Task objects for output.
+     * @return a list of tasks loaded from the file
+     * @throws IOException If an I/O error occurs
+     */
     public List<Task> load() throws IOException {
         ensureExists();
         List<String> lines = Files.readAllLines(filePath, StandardCharsets.UTF_8);
@@ -44,6 +58,12 @@ public class Storage {
         return tasks;
     }
 
+    /**
+     * Saves tasks to the storage file.
+     * This method converts tasks to their string representation and writes them to the file.
+     * @param tasks List of tasks to be saved
+     * @throws IOException If an I/O error occurs
+     */
     public void save(TaskList tasks) throws IOException {
         ensureExists();
         List<String> lines = tasks.asList().stream()
